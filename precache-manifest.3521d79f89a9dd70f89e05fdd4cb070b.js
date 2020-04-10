ddd9cff81c46f237179d381eb2f604ce0affcb45
Vasyl-Hnatyshyn <gnatv7077@gmail.com>
2020-04-10 16:24:01 +0300

    Updates

---
+++ precache-manifest.3521d79f89a9dd70f89e05fdd4cb070b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fa4d41af387582122bbee6a9f5dc7f7",
+    "url": "/TaskManager/index.html"
+  },
+  {
+    "revision": "d1ec6019748635874b27",
+    "url": "/TaskManager/static/css/main.2e238f1c.chunk.css"
+  },
+  {
+    "revision": "c2376775600a4fc6b0fd",
+    "url": "/TaskManager/static/js/2.14a4646d.chunk.js"
+  },
+  {
+    "revision": "a671c0bdee409a39c875ed03e2957619",
+    "url": "/TaskManager/static/js/2.14a4646d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1ec6019748635874b27",
+    "url": "/TaskManager/static/js/main.beb9a27b.chunk.js"
+  },
+  {
+    "revision": "bb4bb32b4e7343ed0e0b",
+    "url": "/TaskManager/static/js/runtime-main.2dcd174f.js"
+  }
+]);
